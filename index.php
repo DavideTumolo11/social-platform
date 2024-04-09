d9ef5df228f46ab36bca19e4371f81e4bbc04e90
DavideTumolo11 <davidetumolo11@gmail.com>
2024-04-09 21:55:04 +0200

    query_php

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+
+define('DB_SERVERNAME', 'localhost:3306');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_NAME', 'social-platform');
+
+$connection = new mysqli(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+
+if ($connection && $connection->connect_error) {
+    echo 'database connection failed';
+    die;
+}
+
+
+$sqlMedia = "SELECT `username` FROM `users` JOIN `medias` ON `users`.`id` = `medias`.`user_id` WHERE `medias`.`type` = 'video' GROUP BY `users`.`username`";
+$resultSqlMedia = $connection->query($sqlMedia);
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+
+    <?php while ($row = $resultSqlMedia->fetch_assoc()) :
+
+        ['username' => $username] = $row;
+
+    ?>
+
+
+        <div>
+            <p>students who have posted at least one video <strong><?= $username ?></strong></p>
+        </div>
+
+
+
+
+    <?php endwhile; ?>
+
+
+</body>
+
+</html>
